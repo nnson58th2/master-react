1efc7eb1ab60b9e588b70c39cc7946f4bb45f78f
Son Nguyen <nhuson2306@gmail.com>
2021-08-04 22:28:14 +0700

    Code UI SortBar

--- shopee/src/pages/Home/Home.jsx
+++ shopee/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import FilterPanel from 'src/components/FilterPanel/FilterPanel'
+import SearchItemResult from 'src/components/SearchItemResult/SearchItemResult'
 import * as S from './home.style'
 
 export default function Home() {
@@ -10,7 +11,9 @@ export default function Home() {
         <S.Side>
           <FilterPanel />
         </S.Side>
-        <S.Main></S.Main>
+        <S.Main>
+          <SearchItemResult />
+        </S.Main>
       </S.Container>
     </div>
   )
