5e5b2528c28fe74c584e34cb2eaa090e039b4b33
Son Nguyen <nhuson2306@gmail.com>
2021-08-20 23:30:58 +0700

    Code UI trang Password

--- shopee/src/pages/User/Password/Password.jsx
+++ shopee/src/pages/User/Password/Password.jsx
@@ -1,5 +1,45 @@
 import React from 'react'
 
+import InputPassword from 'src/components/InputPassword/InputPassword'
+import { PasswordContent } from './password.style'
+import * as S from '../Profile/profile.style'
+
 export default function Password() {
-  return <div>Password</div>
+  return (
+    <S.Profile>
+      <S.ProfileHeader>
+        <S.ProfileHeaderTitle>Đổi mật khẩu</S.ProfileHeaderTitle>
+        <S.ProfileHeaderSubtitle>
+          Để bảo mật tài khoản, vui lòng không chia sẽ mật khẩu cho người khác
+        </S.ProfileHeaderSubtitle>
+
+        <PasswordContent>
+          <S.InputLabel>
+            <S.InputLabelLabel>Mật khẩu cũ</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <InputPassword name="old-password" />
+            </S.InputLabelContent>
+          </S.InputLabel>
+
+          <S.InputLabel>
+            <S.InputLabelLabel>Mật khẩu mới</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <InputPassword name="new-password" />
+            </S.InputLabelContent>
+          </S.InputLabel>
+
+          <S.InputLabel>
+            <S.InputLabelLabel>Nhập lại mật khẩu</S.InputLabelLabel>
+            <S.InputLabelContent>
+              <InputPassword name="confirm-new-password" />
+            </S.InputLabelContent>
+          </S.InputLabel>
+        </PasswordContent>
+
+        <S.Submit>
+          <S.ButtonSubmit type="submit">Lưu</S.ButtonSubmit>
+        </S.Submit>
+      </S.ProfileHeader>
+    </S.Profile>
+  )
 }
---
+++ shopee/src/pages/User/Password/password.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const PasswordContent = styled.form`
+  width: 50rem;
+  padding-top: 2.5rem;
+`
